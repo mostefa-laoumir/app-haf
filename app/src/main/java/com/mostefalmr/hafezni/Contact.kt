4fba6c09cc0c42f7283808d123164be9fbd97c67
mostefa <21nounou21@gmail.com>
2021-04-20 22:02:37 +0100

    first commit

---
+++ app/src/main/java/com/mostefalmr/hafezni/Contact.kt
@@ -0,0 +1,11 @@
+package com.mostefalmr.hafezni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
---
+++ app/src/main/java/com/mostefalmr/hafezni/Schedule.kt
@@ -0,0 +1,11 @@
+package com.mostefalmr.hafezni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Schedule : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
