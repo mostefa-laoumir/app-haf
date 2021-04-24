8b13a8a39c042e66c4641192fc3d60bf6db62a1f
mostefa <21nounou21@gmail.com>
2021-04-24 01:25:09 +0100

    Main almost done

--- app/src/main/java/com/mostefalmr/hafezni/MainActivity.kt
+++ app/src/main/java/com/mostefalmr/hafezni/MainActivity.kt
@@ -2,12 +2,16 @@ package com.mostefalmr.hafezni
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.module_ticket.*
 import kotlinx.android.synthetic.main.module_ticket.view.*
 
 
@@ -18,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         moduleList.add(ModuleModel("Engeneering",14.0))
+        moduleList.add(ModuleModel("Engeneering",50.0))
+        moduleList.add(ModuleModel("Engeneering",92.0))
+        adapter = ModuleAdapter(this, moduleList)
+        modulesList.adapter = adapter
+
+
 
 
     }
@@ -35,7 +45,19 @@ class MainActivity : AppCompatActivity() {
 
                 var myView = inflater.inflate(R.layout.module_ticket, null)
                 myView.moduleID.text = module.name!!
-                myView.percentageID.text = module.percentage.toString()
+                myView.percentageID.text = module.percentage.toString()+"%"
+                if(module.percentage!!<50.0){
+                    myView.moduleRankImage.setImageResource(R.drawable.redmotiv)
+                }else if(module.percentage!!<85.0){
+                    myView.moduleRankImage.setImageResource(R.drawable.yellowmotiv)
+                    myView.percentageID.setTextColor(ContextCompat.getColor(context!!, R.color.hafezni_Gold))
+
+                }else{
+                    myView.moduleRankImage.setImageResource(R.drawable.tealmotiv)
+                    myView.percentageID.setTextColor(ContextCompat.getColor(context!!, R.color.hafezni_Teal))
+
+                }
+
 
                 return myView
 
