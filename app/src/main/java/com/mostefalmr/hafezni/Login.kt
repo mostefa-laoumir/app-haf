757d3f6e86b8fc3d9e29c95d072942542e29c312
mostefa <21nounou21@gmail.com>
2021-04-25 01:32:54 +0100

    finished app

--- app/src/main/java/com/mostefalmr/hafezni/Login.kt
+++ app/src/main/java/com/mostefalmr/hafezni/Login.kt
@@ -1,11 +1,20 @@
 package com.mostefalmr.hafezni
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        button.setOnClickListener{
+            if(editTextTextPersonName.text.toString()=="998877" && editTextTextPassword.text.toString()=="123"){
+                var intent = Intent(this, Mood::class.java)
+                finish()
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/mostefalmr/hafezni/Mood.kt
+++ app/src/main/java/com/mostefalmr/hafezni/Mood.kt
@@ -1,11 +1,38 @@
 package com.mostefalmr.hafezni
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_mood.*
 
 class Mood : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood)
+        var intent = Intent(this, MainActivity::class.java)
+        imageView2.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
+        imageView3.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
+        imageView4.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
+        imageView6.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
+        imageView7.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
+        imageView8.setOnClickListener {
+            finish()
+            startActivity(intent)
+        }
     }
 }
